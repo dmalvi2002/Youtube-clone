2e36da789e858ba0aae4ac49ef649779d8f13c8d
Abu Bakar Siddique <80147468+dmalvi2002@users.noreply.github.com>
2022-08-31 14:52:45 +0600

    Created react player

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,8 +1,34 @@
 import { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import { Typography, Box, Stack } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+import { Videos } from "./export";
+import { fetchFromApi } from '../utils/fetch';
 
 const VideoDetail = () => {
+  const { id } = useParams();
+  const [videoDetail, setvideoDetail] = useState(null);
+
+  useEffect(() => {
+    fetchFromApi(`videos?part=snippet,statistics&id=${id}`)
+      .then(data => setvideoDetail(data.items[0]));
+  }, [id]);
+
   return (
-    <div>VideoDetail</div>
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: 'column', md: 'row' }}>
+        <Box flex={1}>
+          <Box sx={{ width: '100%', position: 'sticky', top: "86px" }}>
+            <ReactPlayer
+              url={`https://www.youtube.com/watch?v=${id}`}
+              className='react-player'
+              controls
+            />
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
   );
 };
 
