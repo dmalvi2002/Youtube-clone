c7da74a2b8626f2479bc816c3e3b4722aa4ee962
Abu Bakar Siddique <80147468+dmalvi2002@users.noreply.github.com>
2022-08-31 14:00:09 +0600

    Loaded content to the dom and styled them.

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,46 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { Videos, ChannelCard } from "./export";
+import { fetchFromApi } from '../utils/fetch';
 
 const ChannelDetail = () => {
+  const { id } = useParams();
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    fetchFromApi(`channels?part=snippet&id=${id}`)
+      .then(data => {
+        console.log(data);
+        return setChannelDetail(data?.items[0]);
+      });
+
+    fetchFromApi(`search?channelId=${id}&part=snippet&order=date`)
+      .then(data => {
+        console.log(data);
+        return setVideos(data?.items);
+      });
+  }, [id]);
+
+
   return (
-    <div>ChannelDetail</div>
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            background: "linear-gradient(90deg, #FC466B 0%, #3F5EFB 100%)",
+            zIndex: 10,
+            height: "300px",
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop="-110px" bgcolor="transparent" />
+      </Box>
+      <Box display="flex" p="2">
+        <Box sx={{ mr: { sm: "100px" } }}>
+          <Videos videos={videos} />
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
